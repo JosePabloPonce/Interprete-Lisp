1422a87788b415e946e99920b7775c4b4c9efd23
Jose Pablo Ponce <46695351+JosePabloPonce@users.noreply.github.com>
2020-03-14 15:42:10 -0600

    Update LeerDatos.java

--- LeerDatos.java
+++ LeerDatos.java
@@ -1,4 +1,31 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class LeerDatos {
+	
+	
+	ArrayList<String> Dicvalores = new ArrayList<String>();
+	
+	//Funcion para leer archivo de texto y pasarlo a array
+	 public ArrayList<String> leerarchivoalista(String nombredearchivodetexto) throws IOException 
+	    {
+	        FileReader leer = new FileReader(nombredearchivodetexto);         
+	        BufferedReader bufferedReader = new BufferedReader(leer);
+	        String leerlinea = null;
+	        int i=0;
+	        
+	        while ((leerlinea = bufferedReader.readLine()) != null) {
+		    Dicvalores.add(leerlinea);		    
+	        i++;
+	        }	         
+	        bufferedReader.close();
+	        return Dicvalores;
+	    }  
+
+
+
+
 
 }
