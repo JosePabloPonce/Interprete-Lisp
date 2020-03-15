0529fffc1ab54a6e0851e2f081c16411dcd0b99c
Jose Pablo Ponce <46695351+JosePabloPonce@users.noreply.github.com>
2020-03-14 18:00:59 -0600

    Update Interprete.java

--- Interprete.java
+++ Interprete.java
@@ -1,4 +1,16 @@
+import java.io.IOException;
 
 public class Interprete {
+	
+	public static void main(String[] args)  {
+		
+		String dede = "(/ 1.8 (- 150 32))";
+		LeerDatos controlador = new LeerDatos();
+		
+		System.out.println(controlador.separarparentesis(dede, "("));
+		
+		
+	}
+
 
 }
