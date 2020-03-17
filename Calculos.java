8dc09f4d9387979dff2b7a0f17708f3b090d3f43
Jose Pablo Ponce <46695351+JosePabloPonce@users.noreply.github.com>
2020-03-17 14:05:40 -0600

    Update Calculos.java

--- Calculos.java
+++ Calculos.java
@@ -1,34 +1,19 @@
 import java.util.ArrayList;
+import java.util.NoSuchElementException;
 import java.util.StringTokenizer;
 
 //funcion para hacer los calculos aritmeticos
 public class Calculos {
 	
-	
-	
-	
 	//arrraylists para guardar variables con dos o mas numeros para operar o solo uno por separado
-	ArrayList<String> calculosmasdeunavariable = new ArrayList<String>();
 	ArrayList<String> calculounavariable = new ArrayList<String>();
 	
 	//resultado final de operaciones aritmeticas
-	int resultadocalculosperaciones;
+	double resultadocalculosperaciones;
 	//contador de espacios
 	int contadorespacios=0;
+	int contadororden =0;
 
-	ArrayList <Integer> calculosmasdeunavariableseparadossuma = new ArrayList<Integer>();
-	ArrayList <Integer> calculosmasdeunavariableseparadosresta = new ArrayList<Integer>();
-	ArrayList <Integer> calculosmasdeunavariableseparadosmultiplicacion = new ArrayList<Integer>();
-	ArrayList <Integer> calculosmasdeunavariableseparadosdivision = new ArrayList<Integer>();
-	
-	
-	public ArrayList<String> getCalculosmasdeunavariable() {
-		return calculosmasdeunavariable;
-	}
-
-	public void setCalculosmasdeunavariable(ArrayList<String> calculosmasdeunavariable) {
-		this.calculosmasdeunavariable = calculosmasdeunavariable;
-	}
 
 	public ArrayList<String> getCalculounavariable() {
 		return calculounavariable;
@@ -38,10 +23,10 @@ public class Calculos {
 		this.calculounavariable = calculounavariable;
 	}
 	
-	
-	
 
-	public void SepararCalculosAritmeticos (ArrayList <String> funcion) {
+	
+	//funcion que recorre el arraylist y opera todos los casos donde encuentre dos numeros
+	public void realizaroperacionesdosvariables (ArrayList <String> funcion) {
 		
 		//recorremos arraylist
 		for(int i=0; i<funcion.size();i++) {
@@ -68,38 +53,268 @@ public class Calculos {
 				}
 				//agregar a array de mas de dosvariables
 				if (contadorespacios> 1) {
-					calculosmasdeunavariable.add(funcion.get(i));
+					
+					StringTokenizer tokens = new StringTokenizer(funcion.get(i));
+					String tokensignosoperacion = tokens.nextToken();
+					int operando1;
+					int operando2;
+
+					while (tokens.hasMoreTokens()) {
+						
+						String siguientetoken = tokens.nextToken();
+						
+						//Chequear si el siguiente token para saber si es otra operacion, si lo es empezar a realizar calculos
+						if (siguientetoken.equals("+")|| siguientetoken.equals("-")|| siguientetoken.equals("*")|| siguientetoken.equals("/")) {
+							tokensignosoperacion = siguientetoken;
+						}
+						//realizar operacion si es suma	
+						if (tokensignosoperacion.equals("+")) {
+							
+							while(!siguientetoken.equals("+")&& !siguientetoken.equals("-")&& !siguientetoken.equals("*")&& !siguientetoken.equals("/")  ) {
+							
+							resultadocalculosperaciones += Double.parseDouble(siguientetoken);
+							
+							//atrapar error de que no hay ningun siguiente token
+							try {
+								siguientetoken= tokens.nextToken();
+
+							}
+							
+							catch(NoSuchElementException errorelemento) {
+								siguientetoken = "+";
+								
+							}
+							
+						}
+						}
+						
+						//realizar operacion si es resta	
+						if (tokensignosoperacion.equals("-")) {
+							int contador =0;
+							while(!siguientetoken.equals("+")&& !siguientetoken.equals("-")&& !siguientetoken.equals("*")&& !siguientetoken.equals("/")  ) {
+							
+								//condicion para realizar resta y darle el primer valor que encuentre para que no reste por cero la primera vez
+								if (contador<1) {
+									resultadocalculosperaciones=Double.parseDouble(siguientetoken);
+									contador++;
+									
+								}
+								else {
+									resultadocalculosperaciones -= Double.parseDouble(siguientetoken);
+
+								}
+								
+							
+							//atrapar error de que no hay ningun siguiente token
+							try {
+								siguientetoken= tokens.nextToken();
+
+							}
+							
+							catch(NoSuchElementException errorelemento) {
+								siguientetoken = "+";
+								
+							}
+							
+						}
+						}
+						
+						//realizar operacion si es multiplicacion	
+						if (tokensignosoperacion.equals("*")) {
+							//darle el valor de 1 para que la primera multiplicacion no sea 0
+							resultadocalculosperaciones =1;
+							while(!siguientetoken.equals("+")&& !siguientetoken.equals("-")&& !siguientetoken.equals("*")&& !siguientetoken.equals("/")  ) {
+							resultadocalculosperaciones *= Double.parseDouble(siguientetoken);
+							
+							//atrapar error de que no hay ningun siguiente token
+							try {
+								siguientetoken= tokens.nextToken();
+
+							}
+							
+							catch(NoSuchElementException errorelemento) {
+								siguientetoken = "+";
+								
+							}
+							
+						}
+						}
+						
+						//realizar operacion si es division	
+						if (tokensignosoperacion.equals("/")) {
+							int contador=0;
+							while(!siguientetoken.equals("+")&& !siguientetoken.equals("-")&& !siguientetoken.equals("*")&& !siguientetoken.equals("/")  ) {
+								
+								//condicion para realizar division y darle el primer valor que encuentre
+								if (contador<1) {
+									resultadocalculosperaciones=Double.parseDouble(siguientetoken);
+									contador++;
+									
+								}
+								else {
+									resultadocalculosperaciones /= Double.parseDouble(siguientetoken);
+
+								}
+							
+							//atrapar error de que no hay ningun siguiente token
+							try {
+								siguientetoken= tokens.nextToken();
+
+							}
+							
+							catch(NoSuchElementException errorelemento) {
+								siguientetoken = "+";
+								
+							}
+							
+						}
+						}
+									
+					}
+					
+					//lista donde se van a almacenar los datos operados
+					calculounavariable.add(tokensignosoperacion + " "+String.valueOf(resultadocalculosperaciones));
+					contadorespacios=0;
+					resultadocalculosperaciones=0;
+				
 				}
-				//agregar a array con operaciones de una variable
+		
+				//lista donde se van a almacenar si solo encuentra un dato para operar
 				else {
 					calculounavariable.add(funcion.get(i));
+					contadorespacios=0;
+					resultadocalculosperaciones=0;
+					
 				}
 			
-			//}
 		}	
 	}
 	
-	//funcion para ya realizar calculos aritmeticos correspondientes
-	public int realizaroperaciones () {
+	
+	//funcion que realiza las operaciones de todos los casos donde hay una variable
+	public Double realizaroperacionesunavariable (ArrayList <String> funcion) {
 		
-		//recorrer arraylist de dos o mas variables
-		for(int i =0; i<calculosmasdeunavariable.size(); i++) {
-			StringTokenizer tokens = new StringTokenizer(calculosmasdeunavariable.get(i));
+		//mientras en la funcion hayan mas de dos numeros para operar
+		while(funcion.size()>=2) {
+		
+			//separar por token penultima posicion del arraylist
+			StringTokenizer tokens = new StringTokenizer(funcion.get(funcion.size()-2));
+			StringTokenizer tokens2 = new StringTokenizer(funcion.get(funcion.size()-1));
+
+			//obtener signo de token de penultima operacion
+			String tokensignosoperacion = tokens.nextToken();
+			
+			//omitir simbolo de operacion del ultimo token
+			tokens2.nextToken();
+			
+			//obtener el valor del ultimo token
+			String siguientetoken2= tokens2.nextToken();
+
+			
+		
 			while (tokens.hasMoreTokens()) {
 				
+				String siguientetoken = tokens.nextToken();
 				
-			}
+				//Chequear si el siguiente token para saber si es otra operacion, si lo es empezar a realizar calculos
+				if (siguientetoken.equals("+")|| siguientetoken.equals("-")|| siguientetoken.equals("*")|| siguientetoken.equals("/")) {
+					tokensignosoperacion = siguientetoken;
+				}
+				//realizar operacion si es suma	
+				if (tokensignosoperacion.equals("+")) {
+					
+					while(!siguientetoken.equals("+")&& !siguientetoken.equals("-")&& !siguientetoken.equals("*")&& !siguientetoken.equals("/")  ) {
+					
+					resultadocalculosperaciones = Double.parseDouble(siguientetoken) +  Double.parseDouble(siguientetoken2) ;
+					
+					//atrapar error de que no hay ningun siguiente token
+					try {
+						siguientetoken= tokens.nextToken();
+
+					}
+					
+					catch(NoSuchElementException errorelemento) {
+						siguientetoken = "+";
+						
+					}
+					
+				}
+				}
+				
+				//realizar operacion si es resta	
+				if (tokensignosoperacion.equals("-")) {
+					
+					while(!siguientetoken.equals("+")&& !siguientetoken.equals("-")&& !siguientetoken.equals("*")&& !siguientetoken.equals("/")  ) {
+					
+					resultadocalculosperaciones = Double.parseDouble(siguientetoken)- Double.parseDouble(siguientetoken2);
+					
+					//atrapar error de que no hay ningun siguiente token
+					try {
+						siguientetoken= tokens.nextToken();
+
+					}
+					
+					catch(NoSuchElementException errorelemento) {
+						siguientetoken = "+";
+						
+					}
+					
+				}
+				}
+				
+				//realizar operacion si es multiplicacion	
+				if (tokensignosoperacion.equals("*")) {
+					//darle el valor de 1 para que la primera multiplicacion no sea 0
+					while(!siguientetoken.equals("+")&& !siguientetoken.equals("-")&& !siguientetoken.equals("*")&& !siguientetoken.equals("/")  ) {
+					resultadocalculosperaciones = Double.parseDouble(siguientetoken) *  Double.parseDouble(siguientetoken2);
+					
+					//atrapar error de que no hay ningun siguiente token
+					try {
+						siguientetoken= tokens.nextToken();
+
+					}
+					
+					catch(NoSuchElementException errorelemento) {
+						siguientetoken = "+";
+						
+					}
+					
+				}
+				}
+				
+				//realizar operacion si es division	
+				if (tokensignosoperacion.equals("/")) {
+					while(!siguientetoken.equals("+")&& !siguientetoken.equals("-")&& !siguientetoken.equals("*")&& !siguientetoken.equals("/")  ) {
+						
+						resultadocalculosperaciones = Double.parseDouble(siguientetoken) / Double.parseDouble(siguientetoken2);
+								
+					
+					//atrapar error de que no hay ningun siguiente token
+					try {
+						siguientetoken= tokens.nextToken();
+
+					}
+					
+					catch(NoSuchElementException errorelemento) {
+						siguientetoken = "+";
+						
+					}
+					
+				}
+				}
 			
+				
+				calculounavariable.set(funcion.size()-2 ,tokensignosoperacion + " "+String.valueOf(resultadocalculosperaciones));
+				funcion.remove(funcion.size()-1);
+				contadororden++;
+				contadorespacios=0;
+
+			}
 		}
-		
-		
-		
-		
-		
-		return contadorespacios;
+		return resultadocalculosperaciones;	
 		
 	}
-	
-	
+		
 	
 }
+
