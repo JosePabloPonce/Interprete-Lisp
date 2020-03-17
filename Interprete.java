ea4678b5cc52392b38f01f848a0ad089e8756474
Jose Pablo Ponce <46695351+JosePabloPonce@users.noreply.github.com>
2020-03-17 14:07:37 -0600

    Update Interprete.java

--- Interprete.java
+++ Interprete.java
@@ -4,11 +4,14 @@ public class Interprete {
 	
 	public static void main(String[] args)  {
 		
-		String dede = "(/ 1.8 (- 150 32))";
+		String dede = "(+ 3 1 3 (+ 4 3(- 2 3(* 2 3 (/ 2 4)) )))";
 		LeerDatos controlador = new LeerDatos();
+		Calculos controlador2 = new Calculos();
 		
-		System.out.println(controlador.separarparentesis(dede, "("));
-		
+		controlador2.realizaroperacionesdosvariables(controlador.separarparentesis(dede, "("));
+		System.out.println(controlador2.getCalculounavariable());
+		System.out.println(controlador2.realizaroperacionesunavariable(controlador2.getCalculounavariable()));
+
 		
 	}
 
