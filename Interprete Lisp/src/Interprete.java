b0337d83d3c6fa59c388934493994478aaa012b4
Gabriel Quiroz <46507325+gabrielquirozz@users.noreply.github.com>
2020-03-20 18:41:44 -0600

    Update Interprete.java

--- Interprete Lisp/src/Interprete.java
+++ Interprete Lisp/src/Interprete.java
@@ -1,28 +1,17 @@
 import java.io.IOException;
 
+
 public class Interprete {
-	
-	public static void main(String[] args) throws IOException  {
-		
-		
-		LeerDatos controlador = new LeerDatos();
-		Calculos controlador2 = new Calculos();
-		Validacion validar = new Validacion();
-		
-		//leer el archivo de texto y guardarlo en variable funcion, creada en clase leerdatos
-		controlador.leerarchivoalista(System.getProperty("user.dir")+ "\\src\\numeros.txt");
-
-		if(validar.sintaxis(controlador.getFuncion()) == true) {
-			controlador.separarparentesis(controlador.getFuncion(), "(");
-			controlador2.realizaroperacionesdosvariables(controlador.getFuncionseparadaparentesis());
-			System.out.println(controlador2.realizaroperacionesunavariable(controlador2.getCalculounavariable()));
-		}
-		if(validar.sintaxis(controlador.getFuncion()) == false) {
-			System.out.println("Error de Sintaxis");
-		}
-	
-	}
-	
-
-
+    public static void main(String args[]) throws IOException {
+        LeerDatos operador = new LeerDatos();
+        if(operador.sintaxis(operador.importText()) == true){
+        	operador.tokenizer(operador.importText());
+        }
+        else {
+        	System.out.println("Error de Sintaxis");
+        }
+        
+            
+    }
+}
 }
