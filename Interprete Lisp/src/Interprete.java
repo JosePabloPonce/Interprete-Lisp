e5e209d590107ffa2b311725eba2ff3fb873ca2d
Gabriel Quiroz <46507325+gabrielquirozz@users.noreply.github.com>
2020-03-20 23:05:07 -0600

    Update Interprete.java

--- Interprete Lisp/src/Interprete.java
+++ Interprete Lisp/src/Interprete.java
@@ -1,11 +1,15 @@
 import java.io.IOException;
+import java.util.Scanner;
 
 
 public class Interprete {
     public static void main(String args[]) throws IOException {
         LeerDatos operador = new LeerDatos();
-        if(operador.sintaxis(operador.importText()) == true){
-        	operador.leeryejecutar(operador.importText());
+        System.out.println("Ingrese el nombre del archivo de texto: \n");
+        Scanner myObj = new Scanner(System.in);
+        String texto = myObj.nextLine();
+        if(operador.sintaxis(operador.importText(texto)) == true){
+        	operador.leeryejecutar(operador.importText(texto));
         }
         else {
         	System.out.println("Error de Sintaxis");
@@ -14,4 +18,3 @@ public class Interprete {
             
     }
 }
-
