ed8dd6b10ce01850ed4e2eae0cbc5523d0dae9cf
Jose Pablo Ponce <46695351+JosePabloPonce@users.noreply.github.com>
2020-03-20 23:33:25 -0600

    Update Interprete.java

--- Interprete Lisp/src/Interprete.java
+++ Interprete Lisp/src/Interprete.java
@@ -1,3 +1,9 @@
+/**
+ * Programa para ejecutar interprete lisp
+ * @author Jose Pablo Ponce 19092, Gabriel Quiroz 19255, Oscar Paredez 19109
+ * @version 20/03/2020
+ */
+
 import java.io.IOException;
 import java.util.Scanner;
 
