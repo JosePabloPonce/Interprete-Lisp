e8f2a12db3b145ccf4cd9d0ae823046bdcd6b613
Jose Pablo Ponce <46695351+JosePabloPonce@users.noreply.github.com>
2020-03-20 20:59:27 -0600

    Update Interprete.java

--- Interprete Lisp/src/Interprete.java
+++ Interprete Lisp/src/Interprete.java
@@ -5,7 +5,7 @@ public class Interprete {
     public static void main(String args[]) throws IOException {
         LeerDatos operador = new LeerDatos();
         if(operador.sintaxis(operador.importText()) == true){
-        	operador.tokenizer(operador.importText());
+        	operador.leeryejecutar(operador.importText());
         }
         else {
         	System.out.println("Error de Sintaxis");
@@ -14,4 +14,4 @@ public class Interprete {
             
     }
 }
-}
+
